a9868141081e08e46af133ba421ec82e4c92e0d3
Jenna Wise <jennawise@hotmail.com>
2018-07-26 14:13:39 -0400

    Annotation of Matrix_ncols; some spacing adjustments in Matrix_nrows
    Not using matrix_fp yet

--- GraphBLAS-wp/Source/GrB_Matrix_ncols.c
+++ GraphBLAS-wp/Source/GrB_Matrix_ncols.c
@@ -1,12 +1,12 @@
 //------------------------------------------------------------------------------
-// GrB_Matrix_nrows: number of rows of a sparse matrix
+// GrB_Matrix_ncols: number of columns of a sparse matrix
 //------------------------------------------------------------------------------
 
 // SuiteSparse:GraphBLAS, Timothy A. Davis, (c) 2017-2018, All Rights Reserved.
 // http://suitesparse.com   See GraphBLAS/Doc/License.txt for license.
 
 // *** JENNA ANNOTATION 7/25/18 ***
-// GrB_Matrix_nrows
+// GrB_Matrix_ncols
 
 //------------------------------------------------------------------------------
 
@@ -14,11 +14,11 @@
 #include "annotlib.h" // for common predicates & logic functions
 
 /*@
- requires \separated(&GB_thread_local,\union(nrows,A)) ;
- requires \separated(GB_thread_local.where,\union(nrows,A)) ;
- requires \separated(GB_thread_local.file,\union(nrows,A)) ;
+ requires \separated(&GB_thread_local,\union(ncols,A)) ;
+ requires \separated(GB_thread_local.where,\union(ncols,A)) ;
+ requires \separated(GB_thread_local.file,\union(ncols,A)) ;
  behavior input_invalid:
-    assumes nrows == \null  ||
+    assumes ncols == \null  ||
             A == \null      ||
             !matrix_valid(A) ;
     assigns GB_thread_local.where ;
@@ -26,45 +26,45 @@
     assigns GB_thread_local.line ;
     assigns GB_thread_local.info ;
     ensures (\result == GrB_NULL_POINTER ==>
-                nrows == \null || A == \null)        ||
+                ncols == \null || A == \null)        ||
             (\result == GrB_INVALID_OBJECT ==>
                 !matrix_valid(A))                    ||
             (\result == GrB_UNINITIALIZED_OBJECT ==>
-                nrows != \null &&
+                ncols != \null &&
                 \valid(A)      &&
                 !matrix_init(A))                     ||
             \result == GrB_PANIC ;
  behavior input_valid_no_alias:
-    assumes nrows != \null ;
+    assumes ncols != \null ;
     assumes A != \null ;
     assumes matrix_valid(A) ;
-    assumes \separated(nrows,A) ;
-    requires \valid(nrows) ;
-    assigns *nrows ;
+    assumes \separated(ncols,A) ;
+    requires \valid(ncols) ;
+    assigns *ncols ;
     assigns GB_thread_local.where ;
     assigns GB_thread_local.info ;
     ensures (\result == GrB_SUCCESS ==>
                 matrix_valid(A)           &&
-                *nrows == matrix_nrows(A) &&
-                \valid(nrows))            ||
+                *ncols == matrix_ncols(A) &&
+                \valid(ncols))            ||
             \result == GrB_PANIC ;
  behavior input_valid_alias:
-    assumes nrows != \null ;
+    assumes ncols != \null ;
     assumes A != \null ;
     assumes matrix_valid(A) ;
-    assumes !\separated(nrows,A) ;
-    requires \valid(nrows) ;
-    assigns *nrows ;
+    assumes !\separated(ncols,A) ;
+    requires \valid(ncols) ;
+    assigns *ncols ;
     assigns GB_thread_local.where ;
     assigns GB_thread_local.info ;
-    ensures (\result == GrB_SUCCESS ==> \valid(nrows)) ||
+    ensures (\result == GrB_SUCCESS ==> \valid(ncols)) ||
             \result == GrB_PANIC ;
  complete behaviors ;
  disjoint behaviors ;
  */
-GrB_Info GrB_Matrix_nrows   // get the number of rows of a matrix
+GrB_Info GrB_Matrix_ncols   // get the number of columns of a matrix
 (
-    GrB_Index *nrows,       // matrix has nrows rows
+    GrB_Index *ncols,       // matrix has ncols columns
     const GrB_Matrix A      // matrix to query
 )
 {
@@ -73,14 +73,19 @@ GrB_Info GrB_Matrix_nrows   // get the number of rows of a matrix
     // check inputs
     //--------------------------------------------------------------------------
 
-    WHERE ("GrB_Matrix_nrows (&nrows, A)") ;
-    RETURN_IF_NULL (nrows) ;
+    WHERE ("GrB_Matrix_ncols (&ncols, A)") ;
+    RETURN_IF_NULL (ncols) ;
     RETURN_IF_NULL_OR_UNINITIALIZED (A) ;
 
+    // zombies and pending tuples have no effect on nrows
+    // but don't bother asserting that fact here
+
     //--------------------------------------------------------------------------
-    // get the number of rows
+    // return the number of columns
     //--------------------------------------------------------------------------
 
-    return (GB_Matrix_nrows (nrows, A)) ;
+    //@ assert matrix_valid(A) ;
+    (*ncols) = A->ncols ;
+    return (REPORT_SUCCESS) ;
 }
 
